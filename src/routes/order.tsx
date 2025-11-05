22105cea69c1e858da87bdde2d28bcde35bc9934
Трофимов Алексей <a.trofimov@domain.local>
2025-11-05 18:00:27 +0300

    wip: order

--- src/routes/order.tsx
+++ src/routes/order.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useForm, type SubmitHandler } from 'react-hook-form'
+import { TextInput, SegmentedControl } from '@mantine/core'
+
 
 type ProductOrderSchema = {
   productId: number
@@ -16,7 +18,9 @@ export const Route = createFileRoute('/order')({
 
 type FormInputs = {
   firstName: string,
-  lastName: string
+  lastName: string,
+  delivery: "courier" | "pickup",
+  acceptRules: boolean
 }
 
 function RouteComponent() {
@@ -28,7 +32,9 @@ function RouteComponent() {
   } = useForm<FormInputs>({
     defaultValues: {
       firstName: "",
-      lastName: ""  
+      lastName: "",
+      delivery: "courier",
+      acceptRules: false
     }
   })
 
@@ -36,18 +42,39 @@ function RouteComponent() {
     console.log(data)
   }
 
+  const nameValidation = { required: true, minLength: 2, pattern: /^[А-Я][а-я]+/ }
+  // const validateDoubleA = (name: string) => name.match(/а{2,}/gi) !== null ? true : false
+
   return (
     <>
       <div>Order for Product {productId}</div>
 
       <form style={{ display: "flex", flexDirection: "column", gap: 10 }} onSubmit={handleSubmit(onSubmit)}>
-        <input placeholder='Имя' {...register("firstName", { required: true })} />
-        {formState.errors.firstName && <span>Поле неверно заполнено</span>}
+        <TextInput label="Имя" placeholder="Иван" />
+        
+        {/* <input placeholder='Имя' {...register("firstName", nameValidation)} />
+        {formState.errors.firstName && <span>Поле неверно заполнено</span>} */}
 
-        <input placeholder='Фамилия' {...register("lastName", { required: true })} />
-        {formState.errors.lastName && <span>Поле неверно заполнено</span>}
+        <TextInput label="Фамилия" placeholder="Иванов" />
 
-        <button type="submit">Оформить</button>
+        {/* <input placeholder='Фамилия' {...register("lastName", nameValidation)} />
+        {formState.errors.lastName && <span>Поле неверно заполнено</span>} */}
+
+        <label htmlFor="delivery">Тип доставки</label>
+        <SegmentedControl data={[
+          { label: "Курьер", value: "courier"},
+          { label: "Самовывоз", value: "pickup"}
+        ]} />
+        
+        {/* <select id="delivery" {...register("delivery", { required: true })}>
+          <option value="courier">Курьер</option>
+          <option value="pickup">Самовывоз</option>
+        </select> */}
+
+        <input type="checkbox" id="acceptRules" {...register("acceptRules", { required: true })} />
+        <label htmlFor="acceptRules">Согласен с <a>правилами доставки</a></label>
+
+        <button type="submit" disabled={!formState.isValid}>Оформить</button>
       </form>
     </>
   )
